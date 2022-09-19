d8f8043cd2b91657571766fc56a683ce968a6249
marisol perez <107561805+marisolnoeliaperez@users.noreply.github.com>
2022-09-18 21:16:08 -0300

    Add files via upload

---
+++ ejerci11/script.js
@@ -0,0 +1,57 @@
+let diaSemana = prompt("¿Qué día es hoy?");
+switch (diaSemana) {
+case "lunes":
+
+alert("Hoy lunes te toca musculacion de tren superior");
+
+break;
+case "martes":
+
+alert("Hoy martes te toca funcional");
+
+break;
+case "miércoles":
+
+alert("Hoy miércoles te toca musculacion tren inferior");
+
+break;
+case "jueves":
+
+alert("Hoy jueves te toca cardio");
+
+break;
+case "viernes":
+
+alert("Hoy vierneste toca caminata");
+
+break;
+case "sábado":
+
+alert("Hoy sábado te toca salir a andar en bici");
+
+break;
+case "domingo":
+
+alert("Hoy domingo te toca descansar");
+
+break;
+default:
+
+alert("Escribe el día de la semana en minúsculas.");}
+
+
+let value = prompt("¿Cuantas veces quieres que aparezca 'Hola Mundo'?");
+
+for (let i = 0; i < value ; i++) {
+  document.write("Hola Mundo: " + i + "<br>"); 
+}
+
+
+for(i=20;i<=70;i++){
+
+    document.write("El número es: " + i + "<br>");
+    
+    }
+
+
+
